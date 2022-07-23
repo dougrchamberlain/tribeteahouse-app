fbaf1b32c41b3dbf8fd3792b416f1d64f93e083a
dougrchamberlain <d.chamberlain@nextech.com>
2022-07-23 11:14:03 -0400

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Marina's App for Tribe StackBlitz!</h1>
+      <p>Start editing to see some magic happen :)</p>
+    </div>
+  );
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ts-ccsrtr
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-ccsrtr)
